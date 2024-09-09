mkadlof/plepiseq-wgs-pipeline

commit 1f5170a49e9171c4bf45be91c662beeccf20f158

WIP: Fixing bugs during setting up jenkins

nf_pipeline_sars.nf
@@ -103,12 +103,12 @@ workflow{
     c1 = filtering.out[1].join(masking.out)
     merging(c1)
     picard(bwa.out)
-    indelQual(merging.out, ref_genome)
-    lowCov(indelQual.out, ref_genome)
-    varScan(indelQual.out, ref_genome)
-    freeBayes(indelQual.out, ref_genome)
-    lofreq(indelQual.out, ref_genome_with_index)
-    wgsMetrics(indelQual.out, ref_genome)
+    indelQual(merging.out.join(ref_genome))
+    lowCov(indelQual.out.join(ref_genome))
+    varScan(indelQual.out.join(ref_genome))
+    freeBayes(indelQual.out.join(ref_genome))
+    lofreq(indelQual.out.join(ref_genome_with_index))
+    wgsMetrics(indelQual.out.join(ref_genome))
     c2 = lowCov.out[1].join(varScan.out).join(freeBayes.out).join(lofreq.out)
     consensus(c2)
     vcf_for_fasta(consensus.out[0], ref_genome, vcf_template)

bin/sarscov2/update.sh
@@ -20,6 +20,7 @@ fi
 if [ "$1" == "nextclade" ]
 then
   /opt/nextclade/bin/nextclade dataset get --name sars-cov-2 --output-zip /home/nextclade/sars-cov-2.zip
+  exit $?
 fi
 
 # Update pangolin
@@ -29,12 +30,14 @@ then
       --target pangolin \
       --upgrade \
       git+https://github.com/cov-lineages/pangolin-data.git
+  exit $?
 fi
 
 # Update kraken2
 if [ "$1" == "kraken" ]
 then
   /home/kraken_updater.py standard /home/kraken
+  exit $?
 fi
 
 # Update freyja
@@ -44,4 +47,5 @@ then
   wget https://raw.githubusercontent.com/andersen-lab/Freyja-data/main/lineages.yml
   wget https://raw.githubusercontent.com/andersen-lab/Freyja-data/main/curated_lineages.json
   wget https://github.com/andersen-lab/Freyja-data/raw/main/usher_barcodes.csv
+  exit $?
 fi

modules/common/freeBayes.nf
@@ -3,8 +3,7 @@ process freeBayes {
     publishDir "${params.results_dir}/${sampleId}/freebayes", mode: 'copy'
 
     input:
-    tuple val(sampleId), path(bam), path(bai)
-    tuple val(sampleId2), path(ref_genome)
+    tuple val(sampleId), path(bam), path(bai), path(ref_genome)
 
     output:
     tuple val(sampleId), path('freebayes.fa')

modules/common/indelQual.nf
@@ -3,8 +3,7 @@ process indelQual {
     publishDir "${params.results_dir}/${sampleId}", mode: 'copy', pattern: "forvariants.bam*"
 
     input:
-    tuple val(sampleId), path(bam), path(bai)
-    tuple val(sampleId2), path(ref_genome)
+    tuple val(sampleId), path(bam), path(bai), path(ref_genome)
 
     output:
     tuple val(sampleId), path('forvariants.bam'), path('forvariants.bam.bai')

modules/common/lofreq.nf
@@ -3,8 +3,7 @@ process lofreq {
     publishDir "${params.results_dir}/${sampleId}/lofreq", mode: 'copy'
 
     input:
-    tuple val(sampleId), path(bam), path(bai)
-    tuple val(sampleId2), path(ref_genome_with_index)
+    tuple val(sampleId), path(bam), path(bai), path(ref_genome_with_index)
 
     output:
     tuple val(sampleId), path('lofreq.fa')

modules/common/lowCov.nf
@@ -2,8 +2,7 @@ process lowCov {
     tag "lowCov:${sampleId}"
 
     input:
-    tuple val(sampleId), path(bam), path(bai)
-    tuple val(sampleId2), path(ref_genome)
+    tuple val(sampleId), path(bam), path(bai), path(ref_genome)
 
     output:
     tuple val(sampleId), path('low_coverage.bed')

modules/common/manta.nf
@@ -35,7 +35,7 @@ process manta {
 
             if [ \${ILE_SV} -gt 0 ]; then
                 cat \${GENOME_FASTA} | bcftools consensus -s - manta_results.vcf.gz  > output_manta.fa
-                sarscov2/insert_SV_python2.py ${consensus_masked_fasta} output_manta.fa output_consensus_masked_SV.fa
+                /home/bin/sarscov2/insert_SV_python2.py ${consensus_masked_fasta} output_manta.fa output_consensus_masked_SV.fa
             else
                 HEADER=`head -1 ${consensus_masked_fasta}`
                 NEW_HEADER=`echo -e "\${HEADER}_SV"`

commit 0c866058ade439866f859526318a7b0ce6f424e2

Fix bug introduced in commit 1f5170a4.

nf_pipeline_sars.nf
@@ -81,6 +81,10 @@ workflow{
     // with constant data (like reference genome or primers). This is done to
     // generalize cases where the reference genome may not be identical for
     // each sample (as is the case in the influenza pipeline, for example).
+    ref_genome = ref_genome
+    .combine(reads).map { _, genome, sampleId, reads ->
+        return [sampleId, genome]
+    }
     ref_genome_with_index = ref_genome_with_index
     .combine(reads).map { _, genome_and_index_files, sampleId, reads ->
         return [sampleId, genome_and_index_files]
@@ -89,6 +93,7 @@ workflow{
     .combine(reads).map { _, primers_and_pairs, sampleId, reads ->
         return [sampleId, primers_and_pairs]
     }
+
     primers = primers_and_pairs.map{ sampleId, files -> [sampleId, files[0]] }
 
     // Processes