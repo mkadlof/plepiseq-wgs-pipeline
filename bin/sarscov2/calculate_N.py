1d53681a86696e39a72265e2d39237bbafa3a506
michallaz <mlazniewski@gmail.com>
2025-07-09 17:17:07 +0200

    Modified output of calculate_N.py when no Ns are observed in a sequence to match the message produced when Ns are present. Closes #5

--- bin/sarscov2/calculate_N.py
+++ bin/sarscov2/calculate_N.py
@@ -15,7 +15,7 @@ with open(out, 'w') as f:
         i = 0  # total number
 
         pos = 1  # indeksowana od 1
-        slownik = {}  # tutaj trzymamy ako klucz gdzie zaczyna sie ciag N i jaka ma dlugosc
+        slownik = {}  # tutaj trzymamy jako klucz gdzie zaczyna sie ciag N i jaka ma dlugosc
         previous = 'A'
         ciag = 0
         for element in str(r.seq).upper():
@@ -46,4 +46,4 @@ with open(out, 'w') as f:
         if slownik:
             f.write(f'Total length of a sequence {r.id} is {len(str(r.seq))}; total numer of "N" is {i} ({i / dlugosc * 100}%); longest stretch of "N" starts at {sorted(slownik.items(), key=lambda x: x[1], reverse=True)[0][0]} and its length is {sorted(slownik.items(), key=lambda x: x[1], reverse=True)[0][1]}\n')
         else:
-            f.write(f"None N's in {r.id} found.\n")
+            f.write(f'Total length of a sequence {r.id} is {len(str(r.seq))}; total numer of "N" is 0; longest stretch of "N" starts at -1 and its length is 0\n')
