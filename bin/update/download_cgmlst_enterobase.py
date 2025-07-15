2aa56b72ed8f76da56fe23a33d012e56feaf8934
michallaz <mlazniewski@gmail.com>
2025-07-15 13:29:56 +0200

    Enhance download_cgmlst_enterobase.py with logging and progress messages

--- bin/update/download_cgmlst_enterobase.py
+++ bin/update/download_cgmlst_enterobase.py
@@ -1,6 +1,5 @@
-from urllib.request import urlopen
+import urllib.request
 from urllib.error import HTTPError
-import urllib
 import base64
 import json
 import os
@@ -9,90 +8,89 @@ import time
 from Bio import SeqIO
 import sys
 from multiprocessing import Pool
+import logging
+from datetime import datetime
 
-API_TOKEN = open('/home/update/enterobase_api.txt').readlines()[0].rstrip()
-DATABASE = sys.argv[1]  # ecoli, yersinia, mcatarrhalis API 2.0
-scheme_name = sys.argv[2]  # according to API 2.0
-scheme_dir = sys.argv[3]  # take a look inside https://enterobase.warwick.ac.uk//schemes/ for names of specific schemes
-cpus = int(sys.argv[4])  # Liczba CPUs uzywanych prz tworzeniu bazy dla blasta
-
+# ---- Logging setup ----
+logging.basicConfig(
+    format='[%(asctime)s] %(levelname)s: %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 def __create_request(request_str):
-    base64string = base64.b64encode('{0}: '.format(API_TOKEN).encode('utf-8'))
-    headers = {"Authorization": "Basic {0}".format(base64string.decode())}
+    base64string = base64.b64encode(f'{API_TOKEN}: '.encode('utf-8'))
+    headers = {"Authorization": f"Basic {base64string.decode()}"}
     request = urllib.request.Request(request_str, None, headers)
     return request
 
-
-def execute_command(polecenie: str):
-    """
-
-    :param polecenie: str, polecenie do wykonania w powloce
-    :return: Polecenie ma zwrocic zwykle jakis plik, wiec sama funkcja nie zwraca nic
-    """
-    wykonanie = subprocess.Popen(polecenie, shell=True, stdout=subprocess.PIPE)
-    wykonanie.communicate()
-    return True
-
+def execute_command(command: str):
+    """Run a shell command and wait for it to finish."""
+    logging.info(f"Executing command: {command}")
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if stdout:
+        logging.debug(stdout.decode())
+    if stderr:
+        logging.debug(stderr.decode())
+    return process.returncode == 0
 
 def run_blast(lista_loci, start, end):
-    # index the file
     for locus in lista_loci[start:end]:
         execute_command(f"gunzip {locus}.fasta.gz")
         execute_command(f"makeblastdb -in {locus}.fasta -dbtype nucl")
     return True
 
-def get_feader(file_path):
-    """
-    Simple function to extract allele number from headears int the fasta file.
-    :return: dict, only one key - allele name, value list of ints (allel versions)
-    """
-    dictionary = {}
-    allele_name=os.path.basename(file_path).replace('.fasta','')
-    dictionary[allele_name] = []
-    for seq_record in SeqIO.parse(file_path, "fasta"):
-        dictionary[allele_name].append(seq_record.id.rsplit('_')[1])
+# ---- Start of script ----
+start_time = datetime.now()
+logging.info(f"Script started at {start_time.strftime('%Y-%m-%d %H:%M:%S')}")
 
-    return dictionary
+API_TOKEN = open('/home/update/enterobase_api.txt').readlines()[0].rstrip()
+DATABASE = sys.argv[1]        # ecoli, yersinia, etc.
+scheme_name = sys.argv[2]     # e.g., Achtman7
+scheme_dir = sys.argv[3]      # e.g., Achtman7
+cpus = int(sys.argv[4])       # number of CPUs
 
 address = f'https://enterobase.warwick.ac.uk/api/v2.0/{DATABASE}/{scheme_name}/loci?limit=10000&scheme={scheme_name}&offset=0'
 lista_loci = []
+
 try:
-    response = urlopen(__create_request(address))
+    logging.info(f"Requesting loci list from EnteroBase database: {DATABASE} ...")
+    response = urllib.request.urlopen(__create_request(address))
     data = json.load(response)
+
     i = 0
     for locus in data['loci']:
         if i % 100 == 0:
             time.sleep(1)
-        #print(locus['locus'])
         i += 1
+
         locus_link = f"https://enterobase.warwick.ac.uk//schemes/{scheme_dir}/{locus['locus']}.fasta.gz"
-            # download the locus_address
-        response_locus = urlopen(locus_link)
-        # REmove all the files with that allele if present
+        response_locus = urllib.request.urlopen(locus_link)
+
+        # Remove existing files matching the locus
         _ = [os.remove(x) for x in os.listdir('.') if locus['locus'] in x]
-        
-        with open(f"{locus['locus']}.fasta.gz", 'wb') as output_profile:
-            output_profile.write(response_locus.read())
+
+        with open(f"{locus['locus']}.fasta.gz", 'wb') as f_out:
+            f_out.write(response_locus.read())
         lista_loci.append(f"{locus['locus']}")
-        #slownik_alleli.update(get_feader(f"{locus['locus']}.fasta"))
 
-
-    # donload the profile file
+    # Download profile file
+    logging.info("Downloading profile file...")
     _ = [os.remove(x) for x in os.listdir('.') if 'profiles.list' in x]
     profile_link = f"https://enterobase.warwick.ac.uk//schemes/{scheme_dir}/profiles.list.gz"
-    response_profile = urlopen(profile_link)
-    with open(f"profiles.list.gz", 'wb') as output_profile:
-        output_profile.write(response_profile.read())
-    execute_command(f"gunzip profiles.list.gz")
-except HTTPError as Response_error:
-    print(f"{Response_error.code} {Response_error.reason}. URL: {Response_error.geturl()}\\n Reason: {Response_error.read()}")
+    response_profile = urllib.request.urlopen(profile_link)
+    with open("profiles.list.gz", 'wb') as f_out:
+        f_out.write(response_profile.read())
+    execute_command("gunzip profiles.list.gz")
 
-#np.save('slownik_wszystkich_alleli.npy', slownik_alleli, allow_pickle=True, fix_imports=True)
+except HTTPError as e:
+    logging.error(f"{e.code} {e.reason}. URL: {e.geturl()} Reason: {e.read()}")
+    sys.exit(1)
 
-# indeks data with makeblastdb, more cpus the better
+# ---- Indexing for BLAST ----
+logging.info("Starting indexing loci for BLAST...")
 pool = Pool(cpus)
-
 lista_indeksow = []
 start = 0
 step = len(lista_loci) // cpus
@@ -100,7 +98,6 @@ step = len(lista_loci) // cpus
 for i in range(cpus):
     end = start + step
     if i == (cpus - 1) or end > len(lista_loci):
-        # if this is a last cpu or yoy passed the last element of a list, use as end len(lista_loci)
         end = len(lista_loci)
     lista_indeksow.append([start, end])
     start = end
@@ -108,12 +105,18 @@ for i in range(cpus):
 jobs = []
 for start, end in lista_indeksow:
     jobs.append(pool.apply_async(run_blast, (lista_loci, start, end)))
+
 pool.close()
 pool.join()
 
+# ---- Final steps ----
 if not os.path.exists('local'):
     os.mkdir('local')
 
 if not os.path.exists('local/profiles_local.list'):
-    execute_command(f"head -1 profiles.list >> local/profiles_local.list")
+    execute_command("head -1 profiles.list >> local/profiles_local.list")
+
+end_time = datetime.now()
+logging.info(f"Script finished at {end_time.strftime('%Y-%m-%d %H:%M:%S')}")
+logging.info(f"Total runtime: {str(end_time - start_time).split('.')[0]}")
 
